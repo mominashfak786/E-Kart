6fd314a4b86e61243addd8cda7d70257c239570e
mominashfak786 <ashfakmomin2001@gmail.com>
2024-11-16 12:52:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Biodata from "./Components/Biodata";
+// import Biodata from "./Components/Biodata";
 import ImageEditor from "./Components/ImageEditor";
 function App() {
   return (
