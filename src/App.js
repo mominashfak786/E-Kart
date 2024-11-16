a69152c49e1d60a915bda67c24750892561c1502
mominashfak786 <ashfakmomin2001@gmail.com>
2024-11-16 12:31:26 +0530

    learnign git

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css'
-
-import Navbar from './Components/Navbar';
-import Home from './Components/HomePage';
+import "./App.css";
+import Biodata from "./Components/Biodata";
+import ImageEditor from "./Components/ImageEditor";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home/>
+      <ImageEditor />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import HomePage from "./Components/HomePage";
-import Navbar from "./Components/Navbar";
-import ProductList from "./Components/ProductList";
-import ProductCategoryList from "./Components/ProductCategoryList";
-import ProductDetail from "./Components/ProductDetail";
-import ProductsByCategory from "./Components/ProductsByCategory";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        <Route
-          path="/products/category/:category_name"
-          element={<ProductsByCategory />}
-        />
-        <Route path="/products/categories" element={<ProductCategoryList />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
